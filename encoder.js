2f36d53b14fe64b813493fc6cd34a6295e1cd84b
Joshua <105608879+Joshua-Repos@users.noreply.github.com>
2024-02-18 21:52:10 -0500

    Update encoder.js
    fixing mistake (still in progress)

--- encoder.js
+++ encoder.js
@@ -2,112 +2,112 @@ let input = prompt("Enter your input")
 let answer;
 switch (input) {
     case "a":
-        let result = answer.concat("b");
+         answer.concat("b");
         break
     case "b":
-        let result = answer.concat("d");
+        answer.concat("d");
         break
     case "c":
-        let result = answer.concat("f");
+        answer.concat("f");
         break
     case "d":
-        let result = answer.concat("h");
+        answer.concat("h");
         break
     case "e":
-        let result = answer.concat("j");
+        answer.concat("j");
         break
     case "f":
-        let result = answer.concat("l");
+         answer.concat("l");
         break
     case "g":
-        let result = answer.concat("n");
+        answer.concat("n");
         break
     case "h":
-        let result = answer.concat("p");
+        answer.concat("p");
         break
     case "i":
-        let result = answer.concat("r");
+        answer.concat("r");
         break
     case "j":
-        let result = answer.concat("t");
+        answer.concat("t");
         break
     case "k":
-        let result = answer.concat("k");
+        answer.concat("k");
         break
     case "l":
-        let result = answer.concat("x");
+        answer.concat("x");
         break
     case "m":
-        let result = answer.concat("z");
+        answer.concat("z");
         break
     case "n":
-        let result = answer.concat("y");
+        answer.concat("y");
         break
     case "o":
-        let result = answer.concat("w");
+        answer.concat("w");
         break
     case "p":
-        let result = answer.concat("u");
+         answer.concat("u");
         break
     case "q":
-        let result = answer.concat("s");
+         answer.concat("s");
         break
     case "r":
-        let result = answer.concat("q");
+        answer.concat("q");
         break
     case "s":
-        let result = answer.concat("o");
+         answer.concat("o");
         break
     case "t":
-        let result = answer.concat("m");
+        answer.concat("m");
         break
     case "u":
-        let result = answer.concat("k");
+         answer.concat("k");
         break
     case "v":
-        let result = answer.concat("i");
+         answer.concat("i");
         break
     case "w":
-        let result = answer.concat("g");
+         answer.concat("g");
         break
     case "x":
-        let result = answer.concat("e");
+        answer.concat("e");
         break
     case "y":
-        let result = answer.concat("c");
+        answer.concat("c");
         break
     case "z":
-        let result = answer.concat("a");
+        answer.concat("a");
         break
     case "0":
-        let result = answer.concat(1);
+         answer.concat(1);
         break
     case "1":
-        let result = answer.concat(3);
+        answer.concat(3);
         break
     case "2":
-        let result = answer.concat(6);
+        answer.concat(6);
         break
     case "3":
-        let result = answer.concat(7);
+        answer.concat(7);
         break
     case "4":
-        let result = answer.concat(9);
+        answer.concat(9);
         break
     case "5":
-        let result = answer.concat(8);
+        answer.concat(8);
         break
     case "6":
-        let result = answer.concat(6);
+        answer.concat(6);
         break
     case "7":
-        let result = answer.concat(4);
+        answer.concat(4);
         break
     case "8":
-        let result = answer.concat(2);
+        answer.concat(2);
         break
     case "9":
-        let result = answer.concat(0);
+        answer.concat(0);
         break
 
 }
